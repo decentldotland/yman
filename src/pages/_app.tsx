ab07c5871f386cbdfdcd54d9d4c4eb050753c4ad
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-11-08 12:15:06 -0500

    :hammer: Add Pixelify Font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Pixelify_Sans } from 'next/font/google'
+
+const pixelify_sans = Pixelify_Sans({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${pixelify_sans.style.fontFamily};
+        }
+      `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  )
 }
